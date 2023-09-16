8cb44401615a3f3f1b4b210be9a903311f748c5b
Yuliia Kucheriavenko <kucherisvenko@gmail.com>
2023-09-16 14:23:20 +0300

    add reviews images

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,36 +1,33 @@
-import {Box, Button, Container, Flex, Heading, Image} from "@chakra-ui/react";
+import {Box, Button, Container, Flex, Heading, Image, Grid,} from "@chakra-ui/react";
 import buttonLeft from '../assets/Buttons/buttonLeft.svg';
 import buttonRight from '../assets/Buttons/buttonRigth.svg';
+import {REVIEWS} from "../constants/reviews.ts";
 
-// @ts-ignore
-function SquareButton({children}) {
-  return (
-    <Button
-      bg="#F2F2F2"
-      border="none"
-      outline="none"
-      padding="0"
-      margin="0"
-      _hover={{ bg: "#97E3F2" }}
-    >
-      {children}
-    </Button>
-  );
-}
 export const Reviews = () => {
   return (
-    <Box  as="section"  py={10}>
+    <Box as="section" py={10}>
       <Container maxW="container.xl">
         <Heading textTransform="uppercase" pl={5} mb={9} textAlign={{ base: "center", lg: "start", }}>
           Відгуки
         </Heading>
-       <Flex gap={6} >
-         <SquareButton >
-           <Image src={buttonLeft} alt="Left Button" />
-         </SquareButton>
-         <SquareButton >
-           <Image src={buttonRight} alt="Right Button" />
-         </SquareButton>
+        <Grid
+          templateColumns={{base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)', lg: 'repeat(4, 1fr)'}}
+          gap={8}
+          justifyItems="center"
+          overflowX="hidden"
+        >
+          {REVIEWS.map((img,) => (
+            <Image src={img} borderRadius={20} key={img.id}/>
+          ))}
+        </Grid>
+
+       <Flex gap={6} justifyContent="center" mt={4} >
+         <Button w="48px" h="48px" bgColor="#F2F2F2" >
+           <Image src={buttonLeft} alt="Left Button" maxW="24px" />
+         </Button>
+         <Button w="48px" h="48px" bgColor="#F2F2F2">
+           <Image src={buttonRight} alt="Right Button" maxW="24px" />
+         </Button>
        </Flex>
       </Container>
     </Box>
---
+++ src/constants/reviews.ts
@@ -0,0 +1,21 @@
+import review_1 from '../assets/Reviews/review_1.jpg'
+import review_2 from '../assets/Reviews/review_2.jpg'
+import review_3 from '../assets/Reviews/review_3.jpg'
+import review_4 from '../assets/Reviews/review_4.jpg'
+import review_5 from '../assets/Reviews/review_5.jpg'
+import review_6 from '../assets/Reviews/review_6.jpg'
+import review_7 from '../assets/Reviews/review_7.jpg'
+import review_8 from '../assets/Reviews/review_8.jpg'
+import review_9 from '../assets/Reviews/review_9.jpg'
+import review_10 from '../assets/Reviews/review_10.jpg'
+import review_11 from '../assets/Reviews/review_11.jpg'
+import review_12 from '../assets/Reviews/review_12.jpg'
+import review_13 from '../assets/Reviews/review_13.jpg'
+import review_14 from '../assets/Reviews/review_14.jpg'
+import review_15 from '../assets/Reviews/review_15.jpg'
+
+export const REVIEWS = [
+  review_1, review_2, review_3, review_4, review_5,
+  review_6, review_7, review_8, review_9, review_10,
+  review_11, review_12, review_13, review_14, review_15,
+]
